438c239d7637621c18c526a068e21e93dc8e4f14
picojazz <picojazzz@gmail.com>
2017-12-15 16:37:59 +0000

    test app on prod

--- src/main/java/com/picojazzemploiapi/demo/security/SecurityConfig.java
+++ src/main/java/com/picojazzemploiapi/demo/security/SecurityConfig.java
@@ -27,10 +27,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/register").permitAll();
-        http.authorizeRequests().antMatchers("/api/**").hasRole("user");
+       // http.authorizeRequests().antMatchers("/api/**").hasRole("user");
 
-                http.csrf().disable();
+                //http.csrf().disable();
 
-                http.httpBasic();
+                //http.httpBasic();
     }
 }
