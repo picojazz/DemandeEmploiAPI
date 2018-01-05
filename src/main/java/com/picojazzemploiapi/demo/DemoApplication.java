4d0baedea050b70012910180016cbbf376bca1ce
picojazz <picojazzz@gmail.com>
2018-01-05 17:13:06 +0000

    test prod

--- src/main/java/com/picojazzemploiapi/demo/DemoApplication.java
+++ src/main/java/com/picojazzemploiapi/demo/DemoApplication.java
@@ -24,26 +24,28 @@ public class DemoApplication {
 		ApplicationContext ctx =
 				SpringApplication.run(DemoApplication.class, args);
 		OfferRepository or = ctx.getBean(OfferRepository.class);
-//		or.save(new Offer("Développeur Fullstack Java Angular 4 H/F","Dans le cadre de la refonte de son portail assurance vie et prévoyance, notre client recherche un développeur expérimenté H/F \n" +
-//				"\n" +
-//				"Vos principales missions consisteront à :\n" +
-//				"Participation aux spécifications techniques et fonctionnelles\n" +
-//				"Participation aux développements en J2EE\n" +
-//				"Rédaction des plans de test de qualification\n" +
-//				"Réalisation des cas\n" +
-//				"Déploiement\n" +
-//				"Assistance recette et MEOP\n" +
-//				"Profil recherché \n" +
-//				"\n" +
-//				"D’une formation informatique de niveau Bac +4/5 (Ecole d’ingénieur, commerce ou Université), vous justifiez d’une expérience d’au moins 5 ans dans ce poste. La connaissance des métiers de l’assurance est un plus. \n" +
-//				"\n" +
-//				"Vous maîtrisez également :\n" +
-//				"Design simple\n" +
-//				"Conception et code Object\n" +
-//				"Techniques de testing (TDD, BDD)\n" +
-//				"Clean code\n" +
-//				"Vous appréciez le travail en équipe et dans un environnement agile.\n" +
-//				"il y a 1 heure - sauvegarder - voir l'offre d'origine","dakar",-17.4730629,14.7115395,"cdi",6000000,"2017-12-27","Sonatel Senegal"));
+		for (int i = 0 ; i < 10 ; i++) {
+			or.save(new Offer("Développeur Fullstack Java Angular 4 H/F", "Dans le cadre de la refonte de son portail assurance vie et prévoyance, notre client recherche un développeur expérimenté H/F \n" +
+					"\n" +
+					"Vos principales missions consisteront à :\n" +
+					"Participation aux spécifications techniques et fonctionnelles\n" +
+					"Participation aux développements en J2EE\n" +
+					"Rédaction des plans de test de qualification\n" +
+					"Réalisation des cas\n" +
+					"Déploiement\n" +
+					"Assistance recette et MEOP\n" +
+					"Profil recherché \n" +
+					"\n" +
+					"D’une formation informatique de niveau Bac +4/5 (Ecole d’ingénieur, commerce ou Université), vous justifiez d’une expérience d’au moins 5 ans dans ce poste. La connaissance des métiers de l’assurance est un plus. \n" +
+					"\n" +
+					"Vous maîtrisez également :\n" +
+					"Design simple\n" +
+					"Conception et code Object\n" +
+					"Techniques de testing (TDD, BDD)\n" +
+					"Clean code\n" +
+					"Vous appréciez le travail en équipe et dans un environnement agile.\n" +
+					"il y a 1 heure - sauvegarder - voir l'offre d'origine", "dakar", -17.4730629, 14.7115395, "cdi", 6000000, "2017-12-27", "Sonatel Senegal"));
+		}
 
 		/*UserRepository ur = ctx.getBean(UserRepository.class);
 		CvRepository cvr = ctx.getBean(CvRepository.class);
