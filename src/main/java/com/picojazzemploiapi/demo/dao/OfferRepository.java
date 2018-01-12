82af54e00fd35b356fefadf40bde8d0469e1ca48
picojazz <picojazzz@gmail.com>
2018-01-12 17:16:37 +0000

    add contract and place to search query

--- src/main/java/com/picojazzemploiapi/demo/dao/OfferRepository.java
+++ src/main/java/com/picojazzemploiapi/demo/dao/OfferRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface OfferRepository extends JpaRepository<Offer,Long> {
-    @Query("select o from Offer o where o.title like :x  order by o.dateCreate desc ")
+    @Query("select o from Offer o where o.title like :x Or o.place like :x Or o.contract like :x Or o.company like :x order by o.dateCreate desc ")
     public List<Offer> searchOffer(@Param("x") String rech);
 
 
